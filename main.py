b2a685715726bd6ea8ca20768f296fab0a9bf720
kz21616 <kz21616@gmail.com>
2026-04-11 15:46:55 +0000

    simple fixes

--- main.py
+++ main.py
@@ -3,38 +3,38 @@ import random
 
 
 class Reel:
-    def __init__ (self, max_value = 9)
-    self.max_value = max_value
+    def __init__ (self, max_value = 9):
+        self.max_value = max_value
 
     def spin(self):
         return random.randint(0,9)
 
-
+   
+   
+  
 class Slotmachine:
     def _init__(self):
         self.r1 = Reel()
         self.r2 = Reel()
         self.r3 = Reel()
-   
+    
+    def evaluate_spin(self,r1, r2 ,r3):
+        if r1 == 0 or r2 == 0 or r3 == 0:
+                print("Lose") 
+        elif r1 == r2 == r3 and r1 != 0:
+                print("Win")
+        else:
+                print("spin Agian")
+
     def play_round(self):
         r1 = self.r1.spin()
         r2 = self.r2.spin()
         r3 = self.r3.spin()
 
         total = r1+r2+r3
-        result = evaluate_spin(self,r1,r2,r3)
+        result = self.evaluate_spin(self,r1,r2,r3)
         return r1, r2, r3,total, result
-
-
-
-
-    def evaluate_spin(self,r1, r2 ,r3)
-        if r1 = 0 or r2 = 0 or r3 = 0:
-            print("Lose") 
-        elif r1 = r2 = r3 and r1 != 0:
-            print("Win")
-        else:
-            print("spin Agian")
+  
 
 def main():
     machine = Slotmachine()
@@ -46,12 +46,14 @@ def main():
         choice = input("Selection:")
 
 
-        if choice == 1:
+        if choice =="1" :
+
+            r1,r2,r3,total,result = machine.play_round
             print(f"Reels: {r1}, {r2}, {r3}")
             print(f"Total: {total}")
             print(f"Results: {result}")
 
-        elif choice == 2
+        elif choice == "2":
             print("Exiting...")
             break
 
