a5d6c5b478f1c91652b9424481cbb639d31c73dd
kz21616 <kz21616@gmail.com>
2026-04-11 15:55:03 +0000

    final fixes

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ class Reel:
         self.max_value = max_value
 
     def spin(self):
-        return random.randint(0,9)
+        return random.randint(0,self.max_value)
 
    
    
   
 class Slotmachine:
-    def _init__(self):
+    def __init__(self):
         self.r1 = Reel()
         self.r2 = Reel()
         self.r3 = Reel()
     
     def evaluate_spin(self,r1, r2 ,r3):
         if r1 == 0 or r2 == 0 or r3 == 0:
-                print("Lose") 
+                return "Lose" 
         elif r1 == r2 == r3 and r1 != 0:
-                print("Win")
+                return "Win"
         else:
-                print("spin Agian")
+                return "spin Agian"
 
     def play_round(self):
         r1 = self.r1.spin()
@@ -32,7 +32,7 @@ class Slotmachine:
         r3 = self.r3.spin()
 
         total = r1+r2+r3
-        result = self.evaluate_spin(self,r1,r2,r3)
+        result = self.evaluate_spin(r1,r2,r3)
         return r1, r2, r3,total, result
   
 
@@ -46,9 +46,9 @@ def main():
         choice = input("Selection:")
 
 
-        if choice =="1" :
+        if choice =="1":
 
-            r1,r2,r3,total,result = machine.play_round
+            r1,r2,r3,total,result = machine.play_round()
             print(f"Reels: {r1}, {r2}, {r3}")
             print(f"Total: {total}")
             print(f"Results: {result}")
